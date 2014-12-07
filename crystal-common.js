46005e1eea149e8c4ac9bdad00ac25b927526818
Cody Balos <cjbalos@gmail.com>
2014-12-07 02:58:50 -0800

    Major code cleanup. Better encapsulation and reduction of global variables.

---
+++ crystal-common.js
@@ -0,0 +1,15 @@
+function crystalCommon(formID, ajaxSubmit) {
+	var crystalInstance = Crystal('contact-us', true);
+	
+	// name field validation 
+	crystalInstance.addField(/[A-Za-z -']$/ , document.getElementById('crystal-someName'));
+
+	// email validation... will validate all emails that adhere to the RFC2822 standard.
+	var re = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+	crystalInstance.addField(re, document.getElementById('crystal-email'))
+
+	// message validation... will match everything but an empty message.
+	crystalInstance.addField(/^(?!\s*$).+/ , document.getElementById('crystal-message'))
+
+	return crystalInstance;
+}
--- crystal.js
+++ crystal.js
@@ -1,148 +1,132 @@
 'use strict';
 
-var crystal = new Object();
-
-function crystalField(regex, domOBJ) {
-	this.domOBJ = domOBJ; 
-	this.regex  = regex;  
-	this.lastState = false;
-	this.timesActive = 0;   
-
-  this.isValid = isValid;   
-  this.wasActive = wasActive; 
-}
-
-function wasActive() {
-	if(this.domOBJ == document.activeElement) {
-		this.timesActive++;
-		return this.timesActive
-	} else {
-			return this.timesActive;
-	}
-}
-
-// Checks if crystalField is valid. Returns true, if 
-// it passes regex test, returns false if it fails. 
-function isValid() {
-	if (this.regex.test(this.domOBJ.value)) {
-		return true;
-	} else {
-			return false;
-		}
-}
-
-// A default object for name validation.
-// Will match anything not blank or containing digits. 
-crystal.someName  = new crystalField( /[A-Za-z -']$/ , document.getElementById('crystal-someName'));
-
-// A default object for email validation.
-// Will validate all emails that adhere to the RFC2822 standard.
-crystal.email = new crystalField( '' , document.getElementById('crystal-email'));
-// Directly changing the value of email.regex
-// becaues passing this huge beastas an param would be messy
-crystal.email.regex = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
-
-// A default object for message validation.
-// Will match everything but an empty message.
-crystal.message  = new crystalField( /^(?!\s*$).+/ , document.getElementById('crystal-message'));
-
-// A default object for a hidden spamecheck crystalField. 
-// Returns false if crystalField is anything but empty.
-crystal.spamcheck = new crystalField( /^(?!\s*$).+/ , document.getElementById('crystal-spamcheck'))
-crystal.spamcheck = {
-	isValid: function() {
-		if (!this.regex(this.domOBJ.value)) {
-			return true;
-		} else {
-			return false;
-		}
-	},
-	lastState: true,
-	wasActive: function() {
-		return 1;
-	}
-}
-
-// Checks if crystalField is valid and adds the CSS
-// classes used to show invalid crystal. Also changes lastState.
-function inlineValidate(crystalField) {
-	if(!crystalField.isValid() && crystalField.wasActive() > 0) {
-			crystalField.domOBJ.className += ' invalid';
-			crystalField.lastState = false;
-		} 
-	else {
-			crystalField.domOBJ.className = crystalField.domOBJ.className.replace( /(?:^|\s)invalid(?!\S)/g , '')
-			crystalField.lastState = true;
-		}
-}
-
-function ajaxSubmitForm(formID) {
-	document.getElementById(formID).addEventListener('submit',
-		function(e) {
-			var alertSuccess = document.querySelectorAll(".alert-success");
-			var alertInvalid = document.querySelectorAll(".alert-invalid");
-			var invalid = document.querySelectorAll(".invalid");
-			var required = document.querySelectorAll(".required");
-
-			e.preventDefault();
-
-			var valid = true;
-			for(var crystalField in crystal) {
-				if(crystal[crystalField].lastState === false) {
-					valid = false;
+/**
+ * [Crystal constructor. This is object represents an entire form.]
+ * @param {string} formID     [The ID of the form without the "#"]
+ * @param {boolean} submitType [Will the form be submitted via ajax? If set to false, form will use standard submit.]
+ */
+function Crystal(formID, ajaxSubmit) {
+	var crystalForm = {
+		formID: formID,
+		ajaxSubmit: ajaxSubmit,
+		fields: [],	
+		addField: function(regex, domOBJ) {
+			var aField = new Field(regex, domOBJ);
+			this.fields.push(aField);
+		},
+		inlineValidate: function () {
+			for(var i = 0; i < this.fields.length; i++) {
+				if(!this.fields[i].isValid() && this.fields[i].wasActive() > 0) {
+						this.fields[i].domOBJ.className += ' invalid';
+						this.fields[i].lastState = false;
 				} 
+				else {
+						this.fields[i].domOBJ.className = this.fields[i].domOBJ.className.replace( /(?:^|\s)invalid(?!\S)/g , '')
+						this.fields[i].lastState = true;
+					}
 			}
+		},
+		ajaxSubmitForm: function(formID) {
+			// this will be bound to the form soon
+			// so store it now
+			var this_Crystal = this;
+			document.getElementById(formID).addEventListener('submit',
+				function(e) {
+					var alertSuccess = document.querySelectorAll(".alert-success");
+					var alertInvalid = document.querySelectorAll(".alert-invalid");
+					var invalid = document.querySelectorAll(".invalid");
+					var required = document.querySelectorAll(".required");
 
-			if(valid === true) {
-				var form = serialize(document.getElementById(formID));
+					e.preventDefault();
 
-				var r = new XMLHttpRequest();
-				r.open('POST', '/', true);
-				r.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-				r.onreadystatechange = function () {
-					if(r.readyState != 4 ||  r.status != 200){
-						return;
+					var valid = true;
+					for(var i = 0; i < this_Crystal.fields.length; i++) {
+						if(this_Crystal.fields[i].lastState === false) {
+							valid = false;
+						} 
+					}
+
+					if(valid === true) {
+						var form = serialize(document.getElementById(formID));
+
+						var r = new XMLHttpRequest();
+						r.open('POST', '/', true);
+						r.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+						r.onreadystatechange = function () {
+							if(r.readyState != 4 ||  r.status != 200){
+								return;
+							}
+							else {
+								for(var i = 0; i < invalid.length; i++){
+									invalid[i].className.replace( /(?:^|\s)invalid(?!\S)/g , '');		
+								}	
+								for(var i = 0; i < alertInvalid.length; i++){
+									alertInvalid[i].style.display = 'none';
+								}
+								for(var i = 0; i < alertSuccess.length; i++){
+									alertSuccess[i].style.display = 'block';
+								}
+							}
+						}
+						r.send(form);
 					}
 					else {
-						for(var i = 0; i < invalid.length; i++){
-							invalid[i].className.replace( /(?:^|\s)invalid(?!\S)/g , '');		
-						}	
 						for(var i = 0; i < alertInvalid.length; i++){
-							alertInvalid[i].style.display = 'none';
+							alertInvalid[i].style.display = 'block';
 						}
-						for(var i = 0; i < alertSuccess.length; i++){
-							alertSuccess[i].style.display = 'block';
+						for(var i = 0; i < required.length; i++){
+							required[i].className += " invalid";
 						}
 					}
-				}
-				r.send(form);
-			}
-			else {
-				for(var i = 0; i < alertInvalid.length; i++){
-					alertInvalid[i].style.display = 'block';
-				}
-				for(var i = 0; i < required.length; i++){
-					required[i].className += " invalid";
-				}
-			}
-	});
+				});
+		},
+		standardSubmitForm: function(formID) {
+			document.getElementById(formID).addEventListener('submit', 
+				function(e) {
+					var valid = true;
+					
+					for(var crystalField in crystal) {
+						if(crystal[crystalField].lastState === false) {
+							e.preventDefault();
+							valid = false;
+						} 
+					}
+				});
+		}		
+	};
+
+	if(ajaxSubmit) {
+		crystalForm.ajaxSubmitForm(formID);
+	}
+
+	return crystalForm;
 }
 
-function standardSubmitForm(formID) {
-	document.getElementById(formID).addEventListener('submit', 
-		function(e) {
-			var valid = true;
-			
-			for(var crystalField in crystal) {
-				if(crystal[crystalField].lastState === false) {
-					e.preventDefault();
-					valid = false;
-				} 
+/**
+ * [Field constuctor. Represents a field of a form]
+ * @param {regex literal} regex  [Regular expression to match the field value to.]
+ * @param {object} domOBJ [Object of the DOM]
+ */
+function Field(regex, domOBJ) {
+	this.domOBJ = domOBJ; 
+	this.regex = regex;
+	this.lastState = false;
+	this.timesActive = 0;
+
+	this.isValid = function() {
+		if (this.regex.test(this.domOBJ.value)) {
+			return true;
+		} else {
+				return false;
 			}
-		});
+	};
+
+  this.wasActive = function() {
+		if(this.domOBJ == document.activeElement) {
+			this.timesActive++;
+			return this.timesActive;
+		} else {
+				return this.timesActive;
+		}
+	};
 }
-
-
-
-
-
